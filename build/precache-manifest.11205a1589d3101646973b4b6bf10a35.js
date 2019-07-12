c9b11d03bca361da653b4efb82c272b9fd764603
frankfeng <17717444584@163.com>
2019-07-12 23:58:52 +0800

    react-hello-world

---
+++ build/precache-manifest.11205a1589d3101646973b4b6bf10a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "830467f11a80ab745b16ce3aa1fbc3dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b957211ecbbce56e082",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0b957211ecbbce56e082",
+    "url": "/static/js/main.4de55277.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Hello world</code>
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          开始学习React
         </a>
       </header>
     </div>
